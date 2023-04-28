7af6528164909a5685b6a519c79bc2d2f161acf4
daniglyk <daniglyk2@icloud.com>
2023-04-28 15:22:00 +0300

    third

--- lab-2/vpc.tf
+++ lab-2/vpc.tf
@@ -230,25 +230,25 @@ resource "aws_lb_listener" "front_end" {
   }
 }
 
-#resource "aws_lb_target_group" "test1" {
-#  name     = "ALB-Terraform1"
-#  port     = 8080
-#  protocol = "HTTP"
-#  vpc_id   = "${module.vpc.vpc_id}"
-#}
+resource "aws_lb_target_group" "test1" {
+  name     = "ALB-Terraform1"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${module.vpc.vpc_id}"
+}
 
-#resource "aws_lb_target_group_attachment" "test1" {
-#  target_group_arn = aws_lb_target_group.test1.arn
-#  target_id        = "${module.ec2_instance.id}"
-#  port             = 8080
-#}
-#resource "aws_lb_listener" "front_end1" {
-#  load_balancer_arn = "${aws_lb.test.arn}"
-#  port              = "8080"
+resource "aws_lb_target_group_attachment" "test1" {
+  target_group_arn = aws_lb_target_group.test1.arn
+  target_id        = "${module.ec2_instance.id}"
+  port             = 8080
+}
+resource "aws_lb_listener" "front_end1" {
+  load_balancer_arn = "${aws_lb.test.arn}"
+  port              = "8080"
 
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = "${aws_lb_target_group.test1.arn}"
-#  }
-#}
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.test1.arn}"
+  }
+}
 
